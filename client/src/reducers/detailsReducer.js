3cf032d6debeda76f3dd70fae6d5538761a682cc
marc-gonzalez <marcgonzalez@outlook.com>
2018-03-28 16:44:27 -0700

    new information for events in details compared to business, still need styling

--- client/src/reducers/detailsReducer.js
+++ client/src/reducers/detailsReducer.js
@@ -7,11 +7,9 @@ const DEFAULT_STATE = {
 export default function (state = DEFAULT_STATE, action) {
     switch (action.type) {
         case types.GET_DETAILS:
-            console.log("Details reducer: ", action.payload);
             const { data } = action.payload;
             return { ...state, data };
         case types.CLEAR_DETAILS:
-            console.log("Details Reducer: CLEAR_DETAILS: ", {...state, data: DEFAULT_STATE.data});
             return {...state, data: DEFAULT_STATE.data};
         default:
             return state;
